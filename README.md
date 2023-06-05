2011a86fadf2d8b4c9db87c4df9c699d47716f1b
zhangjiahao93 <695427431@qq.com>
2023-06-05 11:49:32 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-ezb3jk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-ezb3jk)
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Arco from '@arco-design/mobile-react';
+import '@arco-design/mobile-react/esm/style';
+import DatePickerDemo from './test';
+
+export default function App() {
+  return (
+    <div>
+      <DatePickerDemo />
+    </div>
+  );
+}
